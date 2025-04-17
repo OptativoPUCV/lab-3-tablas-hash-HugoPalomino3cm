1027a836423ededc13e6955dceca904bbd616012
Your Name <you@example.com>
2025-04-16 21:48:52 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,21 +40,18 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-    long pos = hash(key, map->capacity);
+    long pos = hash(key,map->capacity);
 
-    while (map->buckets[pos] != NULL && map->buckets[pos]->key != NULL) {
-        if (is_equal(map->buckets[pos]->key, key)) {
+    while(map->buckets[pos] != NULL && map->buckets[pos]->key){
+        if (is_equal(map->buckets[pos]->key,key)){
             map->buckets[pos]->value = value;
             return;
         }
         pos = (pos + 1) % map->capacity;
     }
 
-    Pair *nuevoPair = createPair(key, value);
+    Pair *nuevoPair = createPair(key,value);
 
-    if (map->buckets[pos] != NULL) {
-        free(map->buckets[pos]); 
-    }
     map->buckets[pos] = nuevoPair;
     map->size++;
 
@@ -103,7 +100,7 @@ void eraseMap(HashMap * map,  char * key) {
     long pos = hash(key,map->capacity);
 
     while(map->buckets[pos] != NULL){
-        if (map->buckets[pos]->key != NULL && is_equal(map->buckets[pos]->key,key)){
+        if (map->buckets[pos]->key != NULL && is_equal(map->buckets[pos]->key,key));{
             free(map->buckets[pos]);
             map->buckets[pos] = NULL;
             map->size--;
